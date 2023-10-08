fe87489ddb38b0ce4413f7500c9961c10d1570ea
Panarin Ilya <cccthafcdh@gmail.com>
2023-10-08 21:18:23 +0700

    3_labs

---
+++ lab_3/tmp/CMakeFiles/tests.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tests.dir/lcs.o"
+  "CMakeFiles/tests.dir/lcs.o.d"
+  "CMakeFiles/tests.dir/lcs_test.o"
+  "CMakeFiles/tests.dir/lcs_test.o.d"
+  "tests"
+  "tests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
