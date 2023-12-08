55a614409bc2f0cb3d822c2a6976f5acfee6b664
Panarin Ilya <cccthafcdh@gmail.com>
2023-12-08 15:28:33 +0700

    add fitness

--- lab_6/second/tmp/CMakeFiles/Makefile.cmake
+++ lab_6/second/tmp/CMakeFiles/Makefile.cmake
@@ -47,5 +47,6 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
+  "CMakeFiles/tests.dir/DependInfo.cmake"
   "CMakeFiles/main.dir/DependInfo.cmake"
   )
